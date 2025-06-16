975e657a0c9dc594520eda047255a9089da7d35f
ljhuang020623 <ljhuang0623@gmail.com>
2025-06-17 03:14:51 +0800

    06/17/2025 daily

--- Easy/1200.py
+++ Easy/1200.py
@@ -1,7 +1,7 @@
 from typing import List
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
-        rr.sort()
+        r.sort()
         low, high = 0, arr[-1] - arr[0]
         while low < high:
             mid = (low + high) // 2

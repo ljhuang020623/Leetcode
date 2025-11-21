190b8dcf41f02c4da0dc1ff85d8d3834f606cf74
ljhuang020623 <ljhuang0623@gmail.com>
2025-11-21 15:45:05 +0900

    fixed repo structure

--- Easy/1200.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-class Solution:
-    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
-        r.sort()
-        low, high = 0, arr[-1] - arr[0]
-        while low < high:
-            mid = (low + high) // 2
-
